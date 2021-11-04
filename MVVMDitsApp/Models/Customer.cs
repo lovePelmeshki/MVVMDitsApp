70945779c45aae49fe11468bd878a452939f7543
lovePelmeshki <love.pelmeshki@gmail.com>
2021-11-04 16:42:58 +0300

    Add project files.

---
+++ MVVMDitsApp/Models/Customer.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+
+namespace MVVMDitsApp.Models
+{
+
+    public class Customer :INotifyPropertyChanged
+    {
+        private string _Name;
+        /// <summary>
+        /// Get or set Customer's name
+        /// </summary>
+        public string Name
+        {
+            get => _Name;
+            set
+            {
+                _Name = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public Customer(string name)
+        {
+            _Name = name;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
---
+++ MVVMDitsApp/ViewModels/CustomerViewModel.cs
@@ -0,0 +1,28 @@
+﻿using MVVMDitsApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace MVVMDitsApp.ViewModels
+{
+    internal class CustomerViewModel
+    {
+        public CustomerViewModel()
+        {
+            _Customer = new Customer("Alex");
+        }
+
+        private Customer _Customer;
+        public Customer Customer
+        {
+            get => _Customer;
+        }
+
+        public void SaveChanges()
+        {
+            Debug.Assert(false, String.Format("{0} was updated", Customer.Name));
+        }
+
+    }
+}
